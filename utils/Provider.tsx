7fdf0569e91fdb16dfb812e19fd7cc5b0c9f5315
Julian <julian@contxts.io>
2023-08-31 17:04:56 +0900

    fix - build err

--- utils/Provider.tsx
+++ utils/Provider.tsx
@@ -1,11 +1,14 @@
 'use client';
+
 import React from 'react';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import ReactQueryClient from './ReactQueryClient';
 
 function Providers({ children }: React.PropsWithChildren) {
-  const [client] = React.useState(ReactQueryClient);
+  const [client] = React.useState(
+    new QueryClient({ defaultOptions: { queries: { staleTime: 5000 } } })
+  );
+
   return (
     <QueryClientProvider client={client}>
       {children}
@@ -13,4 +16,5 @@ function Providers({ children }: React.PropsWithChildren) {
     </QueryClientProvider>
   );
 }
+
 export default Providers;
