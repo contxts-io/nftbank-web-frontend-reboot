35ce0936044d20e04783e78c609d8cfc3e105c6a
Julian <julian@contxts.io>
2023-09-01 16:33:30 +0900

    feat - dark 모드 적용. dark: 로 선언해야 한다

--- components/providers/Provider.tsx
+++ components/providers/Provider.tsx
@@ -2,13 +2,16 @@
 import React from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import ReactQueryClient from './ReactQueryClient';
+import ReactQueryClient from '@/utils/ReactQueryClient';
+import { ThemeProvider } from './ThemeProvider';
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = React.useState(ReactQueryClient);
   return (
     <QueryClientProvider client={client}>
-      {children}
+      <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+        {children}
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
