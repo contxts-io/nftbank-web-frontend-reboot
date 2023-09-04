de2607351a84dde0ad16f0106643eb6fb6966882
Julian <julian@contxts.io>
2023-09-04 11:23:33 +0900

    feat - wagmi 세팅. jotai 세팅

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,27 @@
+'use client';
 import { ThemeSwitcher } from '@/components/buttons/ThemeSwitcher';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import { useAccount, useConnect, useEnsName } from 'wagmi';
+import { useAtom, useAtomValue } from 'jotai';
+import { testAtom } from '@/store/test';
 
 const Page = () => {
+  const { address, isConnected } = useAccount();
+  const [test, setTest] = useAtom(testAtom);
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  });
+  const handleChangeAtom = (e: any) => {
+    setTest('changed');
+  };
+  if (!isConnected)
+    return <button onClick={() => connect()}>Connect Wallet</button>;
   return (
-    <div>
+    <div className='bg-primary'>
       <p className='dark:text-red-300'>about</p>
+      <p>{address}</p>
+      <p>{test}</p>
+      <button onClick={handleChangeAtom}>change test</button>
       <ThemeSwitcher />
     </div>
   );
--- components/providers/Provider.tsx
+++ components/providers/Provider.tsx
@@ -3,15 +3,34 @@ import React from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactQueryClient from '@/utils/ReactQueryClient';
+import { Provider as JotaiProvider } from 'jotai';
 import { ThemeProvider } from './ThemeProvider';
+
+import { WagmiConfig, configureChains, createConfig, mainnet } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+});
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = React.useState(ReactQueryClient);
   return (
     <QueryClientProvider client={client}>
-      <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-        {children}
-      </ThemeProvider>
+      <JotaiProvider>
+        <WagmiConfig config={config}>
+          <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+            {children}
+          </ThemeProvider>
+        </WagmiConfig>
+      </JotaiProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
