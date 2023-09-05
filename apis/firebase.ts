8bfe9936a54a60aa81afaded4631ca1a37fc35bb
Julian <julian@contxts.io>
2023-09-05 18:14:51 +0900

    feat - provider 세팅. ssr 로 signin 호출 하기위한 authProvider 생성

---
+++ apis/firebase.ts
@@ -0,0 +1,72 @@
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  sendPasswordResetEmail,
+  confirmPasswordReset,
+  fetchSignInMethodsForEmail,
+} from 'firebase/auth';
+import { auth } from '@/utils/firebase/config';
+
+import errorMessage from '@/utils/errorMessage';
+import { FirebaseError } from '@firebase/app';
+
+const provider = new GoogleAuthProvider();
+
+export const createFirebaseWithEmail = async (email: string, password: string) => {
+  const idToken = await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    return userCredential.user?.getIdToken()
+    }).catch((error) => {
+      throw { ...error, message: errorMessage(error?.code) };
+    });
+  return idToken;
+};
+export const getIdTokenByEmail = async (email: string, password: string) => {
+  return await signInWithEmailAndPassword(auth, email, password)
+    .then((result) => {
+      return result?.user?.getIdToken();
+    })
+    .catch((error) => {
+      throw { ...error, message: errorMessage(error?.code) };
+    });
+};
+export const getSignInMethods = async (email: string) => {
+  return await fetchSignInMethodsForEmail(auth, email)
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      throw { ...error, message: errorMessage(error?.code) };
+    });
+}
+export const getIdTokenByGoogle = async () => {
+  return await signInWithPopup(auth, provider)
+    .then((result) => {
+      return result.user?.getIdToken();
+    })
+    .catch((error: FirebaseError) => {
+      if (error.code !== 'auth/popup-closed-by-user') {
+        throw { ...error, message: errorMessage(error?.code) };
+      }
+    });
+};
+export const sendPasswordReset = async (email: string) => {
+  return await sendPasswordResetEmail(auth, email)
+    .then(() => {
+      return true;
+    })
+    .catch((error) => {
+      throw { ...error, message: errorMessage(error?.code) };
+    });
+};
+export const updatePassword = async (oobCode: string, newPassword: string) => {
+  return await confirmPasswordReset(auth, oobCode, newPassword)
+    .then(() => {
+      return true;
+    })
+    .catch((error) => {
+      throw { ...error, message: errorMessage(error?.code) };
+    });
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/v1/:path*',
+        destination: `${API_BASE_URL}/v1/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
