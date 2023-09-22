ea5d65c5dd62ae5f55c6db8e5320519a858179bc
Julian <julian@contxts.io>
2023-09-22 13:21:43 +0900

    feat - global nav 개발중

---
+++ components/GlobalNavigation.tsx
@@ -0,0 +1,59 @@
+import Image from 'next/image';
+import styles from './GlobalNavigation.module.css';
+import Link from 'next/link';
+import { twMerge } from 'tailwind-merge';
+import { ThemeSwitcher } from './buttons/ThemeSwitcher';
+import NFTBankIcon from '@/public/icon/NFTBankIcon';
+import NFTBankLogo from '@/public/logo/NFTBankLogo';
+
+const GlobalNavigation = () => {
+  return (
+    <nav className={styles.navigation}>
+      <div className='flex items-center'>
+        <div className='flex items-center mr-26'>
+          <Image
+            src={'/icon/nftbank_icon.svg'}
+            width={20}
+            height={20}
+            alt='nftbank logo'
+          />
+          <NFTBankLogo />
+        </div>
+        <Link
+          href={'/portfolio'}
+          className={twMerge(`font-body02-medium ${styles.link}`)}
+        >
+          Portfolio
+        </Link>
+        <Link
+          href={'/watchlist'}
+          className={twMerge(`font-body02-medium ${styles.link}`)}
+        >
+          Watchlist
+        </Link>
+        <Link
+          href={'/activitys'}
+          className={twMerge(`font-body02-medium ${styles.link}`)}
+        >
+          Activitys
+        </Link>
+        <Link
+          href={'/report'}
+          className={twMerge(`font-body02-medium ${styles.link}`)}
+        >
+          Report
+        </Link>
+        <Link
+          href={'/settings'}
+          className={twMerge(`font-body02-medium ${styles.link}`)}
+        >
+          Settings
+        </Link>
+      </div>
+      <div>
+        <ThemeSwitcher />
+      </div>
+    </nav>
+  );
+};
+export default GlobalNavigation;
