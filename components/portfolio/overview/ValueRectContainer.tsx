ff7060961752b9c70dd21633204fa1213d2dd044
Julian <julian@contxts.io>
2023-10-11 11:57:59 +0900

    feat - className 수정

--- components/portfolio/overview/ValueRectContainer.tsx
+++ components/portfolio/overview/ValueRectContainer.tsx
@@ -19,10 +19,10 @@ const ValueRectContainer = () => {
         <div className={styles.row}>
           <p className={`font-subtitle02-bold ${styles.title}`}>$173,398</p>
           <article className={styles.valueSubRect}>
-            <div className={`${styles.valueSubBox} ${styles.plus}`}>
+            <div className={`${styles.diffBox} ${styles.plus}`}>
               <p>-2,117(2.3%)</p>
             </div>
-            <div className={`${styles.valueSubBox}`}>
+            <div className={`${styles.diffBox}`}>
               <p>24h</p>
             </div>
           </article>
