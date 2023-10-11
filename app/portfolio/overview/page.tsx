f7d49d1df72b399119a07278ed7e110540f32ff8
julianEom <90600004+julianEom@users.noreply.github.com>
2023-10-11 20:01:04 +0900

    feat - 액티비티 컨테이너 UI (#53)

--- app/portfolio/overview/page.tsx
+++ app/portfolio/overview/page.tsx
@@ -1,11 +1,16 @@
 import ValueRectContainer from '@/components/portfolio/overview/ValueRectContainer';
 import HistoricalTrendContainer from '@/components/portfolio/overview/HistoricalTrendContainer';
+import RecentActivityContainer from '@/components/portfolio/overview/RecentActivityContainer';
 
 const OverviewPage = () => {
   return (
     <section className='px-24 py-22'>
       <ValueRectContainer />
       <HistoricalTrendContainer />
+      <div className='w-full grid grid-cols-2 gap-x-[20px]'>
+        <RecentActivityContainer />
+        <RecentActivityContainer />
+      </div>
     </section>
   );
 };
--- interfaces/constants.ts
+++ interfaces/constants.ts
@@ -1,4 +1,4 @@
 export type TCurrency = 'ETH' | 'USD';
-export type TChain = 'ethereum'
+export type TChain = 'ethereum' | 'polygon'
 export type TStandard = 'erc721'
 export type TValuationType = 'COLLECTION_FLOOR_PRICE'|'ESTIMATED_PRICE'|'TRAIT_FLOOR_PRICE'
