7585530692821701162d764aee11f260a0973d63
julianEom <90600004+julianEom@users.noreply.github.com>
2023-10-12 14:09:12 +0900

    feat - performance chart container UI 개발 (#54)

--- app/portfolio/overview/page.tsx
+++ app/portfolio/overview/page.tsx
@@ -1,14 +1,15 @@
 import ValueRectContainer from '@/components/portfolio/overview/ValueRectContainer';
 import HistoricalTrendContainer from '@/components/portfolio/overview/HistoricalTrendContainer';
 import RecentActivityContainer from '@/components/portfolio/overview/RecentActivityContainer';
+import PerformanceContainer from '@/components/portfolio/overview/PerformanceContainer';
 
 const OverviewPage = () => {
   return (
-    <section className='px-24 py-22'>
+    <section className='pt-20 px-24 pb-40'>
       <ValueRectContainer />
       <HistoricalTrendContainer />
       <div className='w-full grid grid-cols-2 gap-x-[20px]'>
-        <RecentActivityContainer />
+        <PerformanceContainer />
         <RecentActivityContainer />
       </div>
     </section>
