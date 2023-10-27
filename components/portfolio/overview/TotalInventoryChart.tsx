58d3377b3dd253f2b09371cbb5357998df2e01f2
Julian <julian@contxts.io>
2023-10-27 17:48:06 +0900

    feat - others 부분 추가

--- components/portfolio/overview/TotalInventoryChart.tsx
+++ components/portfolio/overview/TotalInventoryChart.tsx
@@ -1,7 +1,6 @@
 import dynamic from 'next/dynamic';
 import styles from './TotalInventoryChart.module.css';
 import { renderToString } from 'react-dom/server';
-import { twMerge } from 'tailwind-merge';
 import {
   useInventoryCollectionPositionAmount,
   useInventoryCollectionPositionValue,
@@ -12,14 +11,25 @@ import { useAtomValue } from 'jotai';
 import { currencyAtom } from '@/store/currency';
 import { formatPercent, shortenAddress } from '@/utils/common';
 const ApexCharts = dynamic(() => import('react-apexcharts'), { ssr: false });
-const COLORS = ['#2563EB', '#5EEAD4', '#A855F7', '#14B8A6', '#93C5FD'];
+const COLORS = [
+  'var(--color-chart-information)',
+  'var(--color-chart-accent-teal-boldest)',
+  'var(--color-chart-accent-purple-bold)',
+  'var(--color-chart-accent-teal-bold)',
+  'var(--color-chart-accent-blue-boldest)',
+  'var(--color-chart-accent-gray-bolder)',
+];
 const tooltip = ({ series, seriesIndex, dataPointIndex, w, selected }: any) => {
   const color = w.globals.colors[seriesIndex];
   const label = w.globals.labels[seriesIndex];
   return (
     <div className='font-caption-regular py-8 px-16 flex flex-col items-start border-1 border-[var(--color-border-bold)] bg-[var(--color-elevation-surface)]'>
       <div className='w-full flex items-center'>
-        <div className={`${styles[`dot--${seriesIndex}`]} w-8 h-8 mr-8`} />
+        <div
+          className={`${
+            styles[`dot--${seriesIndex % series.length}`]
+          } w-8 h-8 mr-8`}
+        />
         <p className={`text-[var(--color-text-subtle)]`}>{label}</p>
       </div>
       {selected === 'amount' && (
