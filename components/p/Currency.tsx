3719676a7d80fd66173811b68b3e933058ca5383
Julian <julian@contxts.io>
2024-01-22 16:11:53 +0900

    feat - NB23-1534 수정. 두개 (5,8번)남음.

--- components/p/Currency.tsx
+++ components/p/Currency.tsx
@@ -4,13 +4,13 @@ type Props = {
 };
 const CurrencyComponent = (props: Props) => {
   const mark = props.value.substring(0, 1);
-  const bigInt = props.value.substring(1).split('.')[0];
-  const smallInt = props.value.substring(1).split('.')[1];
+  const bigInt = props.value.substring(1).split('.')[0] || '0';
+  const smallInt = props.value.substring(1).split('.')[1] || '00';
   return (
     <p className={`flex items-center ${props.className}`}>
       <span className='text-[var(--color-text-subtlest)]'>{mark}</span>
       <span className='text-[var(--color-text-main)]'>{bigInt}</span>
-      <span className='text-[var(--color-text-subtlest)]'>{smallInt}</span>
+      <span className='text-[var(--color-text-subtlest)]'>{`.${smallInt}`}</span>
     </p>
   );
 };
