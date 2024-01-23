2e131b8bbfa11323179024224576d9d004a1d935
Julian <julian@contxts.io>
2024-01-23 10:04:05 +0900

    feat - error page 디자인 수정. refresh, contact  원래대로 바꿈

--- app/error.tsx
+++ app/error.tsx
@@ -1,10 +1,13 @@
 'use client';
 import Button from '@/components/buttons/Button';
-import Link from 'next/link';
+import siteMetadata from '@/utils/siteMetadata';
 import { useRouter } from 'next/navigation';
-
+import styles from './error.module.css';
 export default function NotFound() {
   const router = useRouter();
+  const onClickContactUs = () => {
+    window.open(`mailto:${siteMetadata.contactEmail}`, '_blank');
+  };
   return (
     <div className='w-full h-full flex flex-col items-center justify-center'>
       <img
@@ -17,10 +20,18 @@ export default function NotFound() {
       <p className='font-subtitle02-regular text-[var(--color-text-main)] mt-27 mb-40'>
         Sorry, unexpected error
       </p>
-      <Link href='/'>Go to Home</Link>
-      <Button onClick={() => router.push('/')} id=''>
-        Go to Home
-      </Button>
+      <div className='flex gap-x-8'>
+        <Button onClick={() => location.reload()} id=''>
+          Try again
+        </Button>
+        <Button
+          id=''
+          className={styles.contactButton}
+          onClick={onClickContactUs}
+        >
+          Contact Us
+        </Button>
+      </div>
     </div>
   );
 }
