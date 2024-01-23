c2c1d2730df149a8504a05b88531b06257d40300
Julian <julian@contxts.io>
2024-01-24 02:14:10 +0900

    feat - live data 표시 삭제

--- components/GlobalFooter.tsx
+++ components/GlobalFooter.tsx
@@ -21,7 +21,7 @@ const GlobalFooter = () => {
         <li>Feedback</li>
         <li>Blog</li>
       </ul>
-      <div className='ml-auto flex items-center gap-x-8'>
+      {/* <div className='ml-auto flex items-center gap-x-8'>
         <div className='border-1 border-[var(--color-border-success)] rounded-full w-16 h-16 relative'>
           <div
             className={`absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] rounded-full animateBlink w-6 h-6 mr-4 bg-[var(--color-background-success-bold)]`}
@@ -30,7 +30,7 @@ const GlobalFooter = () => {
         <p className='font-caption-regular text-[var(--color-text-success)]'>
           LIVE DATA
         </p>
-      </div>
+      </div> */}
     </footer>
   );
 };
