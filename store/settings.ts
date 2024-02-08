e8b3d8f4521e04d53db94c8d9ea5d6161ae5afe4
Julian <julian@contxts.io>
2024-02-08 13:25:00 +0900

    feat - cardview default 설정

--- store/settings.ts
+++ store/settings.ts
@@ -3,7 +3,7 @@ import { TChain } from "@/interfaces/constants";
 import { atom } from "jotai";
 
 export const inventoryTypeAtom = atom<'collection' | 'item'>('collection');
-export const inventoryItemViewTypeAtom = atom<'listView' | 'cardView'>('listView');
+export const inventoryItemViewTypeAtom = atom<'listView' | 'cardView'>('cardView');
 export const openModalAtom = atom<'walletConnect' | 'walletEdit' | 'walletDelete' | 'walletGroup' | null>(null);
 export const myDefaultPortfolioAtom = atom<BasicParam | null>(null);
 export const selectedChainAtom = atom< TChain | 'all'>('all');
