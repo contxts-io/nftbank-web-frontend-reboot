bcbc2cf9bbdb007fd45c8bd2be4841d25cf3e48f
Chris Song <sjhshy@gmail.com>
2024-02-08 15:40:52 +0900

    add tax-filing page

---
+++ app/taxFiling/page.tsx
@@ -0,0 +1,6 @@
+const TaxFilingPage = () => {
+  return (
+    <iframe src="https://nftbank.retool.com/embedded/public/a8da91ed-5f02-4af0-9870-7d2a29fe6d07" width="100%" height="100%"></iframe>
+  );
+};
+export default TaxFilingPage;
