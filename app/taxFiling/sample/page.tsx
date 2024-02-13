8a792012da6e096892615953fba754ea6624d9e4
Julian <julian@contxts.io>
2024-02-13 10:53:15 +0900

    feat - taxfiling 안보이게 하기

--- app/taxFiling/sample/page.tsx
+++ app/taxFiling/sample/page.tsx
@@ -1,10 +1,6 @@
 import Sample from '@/components/taxFiling/Sample';
 
 const TaxFilingPage = () => {
-  return (
-    <div>
-      <Sample />
-    </div>
-  );
+  return <div>{/* <Sample /> */}</div>;
 };
 export default TaxFilingPage;
