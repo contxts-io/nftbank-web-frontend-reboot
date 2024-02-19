50f6368c60dff09528d3b07eba873d7d968fb7c4
Julian <julian@contxts.io>
2024-02-19 16:36:42 +0900

    hotfix - gtmId 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -140,7 +140,7 @@ export default async function RootLayout({
             }); var f = d.getElementsByTagName(s)[0],
                 j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
                     'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', ${GTM_ID});`,
+        })(window, document, 'script', 'dataLayer', '${GTM_ID}');`,
           }}
         />
       </head>
