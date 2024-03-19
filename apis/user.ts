56ed9cf48c5daa22d16b9fa737f3affc4fe759a0
Julian <julian@contxts.io>
2024-03-19 18:12:18 +0900

    feat - user 검색시 u= 인 부분 nickname 으로 통일

--- apis/user.ts
+++ apis/user.ts
@@ -2,6 +2,6 @@ import { TUser } from "@/interfaces/user";
 import instance from "@/utils/axiosInterceptor"
 export const getUser = async<T = TUser>(nickname?:string) => {
   // const {data} = await instance.get<{data:T}>(nickname ? `/user?nickname=${nickname}` : `/auth/me`);
-  const {data} = await instance.get<{data:T}>(`/user?u=${nickname}`);
+  const {data} = await instance.get<{data:T}>(`/user?nickname=${nickname}`);
   return data.data;
 }
