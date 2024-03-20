af6a6add5103b5f0145eb53bc9930d02afdbed2b
Julian <julian@contxts.io>
2024-03-20 21:49:24 +0900

    feat - sample 인벤토리, 아날리시스 페이지 생성

---
+++ app/portfolio/analysis/sample/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+import AcquisitionType from '@/components/portfolio/analysis/AcquisitionType';
+import PerformanceSection from '@/components/portfolio/analysis/PerformanceSection';
+import RealizedGainAndLoss from '@/components/portfolio/analysis/RealizedGainAndLoss';
+import { portfolioUserAtom } from '@/store/portfolio';
+import { myDefaultPortfolioAtom } from '@/store/settings';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { useEffect } from 'react';
+
+const AnalysisPage = () => {
+  // const myPortfolio = useAtomValue(myDefaultPortfolioAtom);
+  // const setPortfolioUser = useSetAtom(portfolioUserAtom);
+  // useEffect(() => {
+  //   setPortfolioUser({
+  //     networkId: 'ethereum',
+  //     ...myPortfolio,
+  //   });
+  // }, [myPortfolio]);
+  return (
+    <section className='pt-20 px-24 pb-40'>
+      <PerformanceSection />
+      <RealizedGainAndLoss />
+      <AcquisitionType />
+    </section>
+  );
+};
+export default AnalysisPage;
