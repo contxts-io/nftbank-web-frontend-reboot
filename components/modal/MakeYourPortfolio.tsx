d02761bd3909393e00508ad0c487e8da907f1f15
Julian <julian@contxts.io>
2024-03-20 20:18:17 +0900

    feat - 하단 모달

--- components/modal/MakeYourPortfolio.tsx
+++ components/modal/MakeYourPortfolio.tsx
@@ -1,18 +1,41 @@
+import { useRouter } from 'next/navigation';
 import Button from '../buttons/Button';
-
-const MakeYourPortfolio = () => {
+import CloseX from '@/public/icon/CloseX';
+type Props = {
+  onClose: () => void;
+};
+const MakeYourPortfolio = (props: Props) => {
+  const router = useRouter();
+  const handleClickButton = () => {
+    router.push('/auth/signin');
+    props.onClose();
+  };
   return (
     <article className='w-[320px] rounded-[12px] py-30 px-20 border-[var(--color-border-bold)] bg-[var(--color-elevation-surface-overlay)] flex flex-col'>
       <div className='w-full flex flex-col gap-y-16'>
-        <p className='font-button02-medium text-[var(--color-text-main)]'>
-          📊 Make your portfolio in NFTBank!
-        </p>
+        <div className='w-full flex items-start'>
+          <p className='font-button02-medium text-[var(--color-text-main)] flex-1'>
+            📊 Make your portfolio in NFTBank!
+          </p>
+          <Button
+            id=''
+            onClick={() => props.onClose()}
+            className='!border-0 ml-auto !p-0 !w-16 !h-16'
+          >
+            <CloseX />
+          </Button>
+        </div>
         <p className='font-caption-medium text-[var(--color-text-subtle)]'>
           {`Connect all your wallets to create the most accurate NFT portfolio : )`}
         </p>
       </div>
-      <Button className='w-full bg-[var(--color-background-brand-bold)] rounded-[4px]'>
-        Get Started
+      <Button
+        className='w-full bg-[var(--color-background-brand-bold)] rounded-[4px] mt-40 flex justify-center items-center'
+        onClick={handleClickButton}
+      >
+        <p className='font-caption-regular text-[var(--color-text-inverse)]'>
+          Get Started
+        </p>
       </Button>
     </article>
   );
