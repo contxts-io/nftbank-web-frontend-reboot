0d693db27690132ba4b784156aa9602db3c0dafe
Julian <julian@contxts.io>
2024-03-21 14:41:45 +0900

    feat - settings page title 설정.

--- app/settings/account/page.tsx
+++ app/settings/account/page.tsx
@@ -1,5 +1,10 @@
 import AccountContainer from '@/components/settings/account/AccountContainer';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Account Settings | NFTBank',
+  description: 'NFTBank.ai - Account Settings',
+};
 const AccountPage = () => {
   return (
     <div className='flex-1 flex justify-center py-24'>
